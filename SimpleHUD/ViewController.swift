094e00814a9b22f837db4dd95be0be4f075fa7ac
Carlos P <cpages.tr@gmail.com>
2014-08-28 19:22:30 +0100

    Initial Commit

---
+++ SimpleHUD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleHUD
+//
+//  Created by Carlos Pages on 28/08/2014.
+//  Copyright (c) 2014 k4Nt30. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SimpleHUDTests/SimpleHUDTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleHUDTests.swift
+//  SimpleHUDTests
+//
+//  Created by Carlos Pages on 28/08/2014.
+//  Copyright (c) 2014 k4Nt30. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SimpleHUDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
